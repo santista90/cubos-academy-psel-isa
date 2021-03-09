3471bc25720031b48e6c44a56186e0f3ede0f20e
Luiz Felipe <luizfelipe90@hotmail.com>
2021-03-09 20:28:56 -0300

    Derrapagem

---
+++ derrapagem.js
@@ -0,0 +1,6 @@
+function solucao(velocidade, aceleracao) {
+    let resVelocidade = (-velocidade*velocidade);
+	let distancia = resVelocidade / (2 * aceleracao) ;
+    console.log(distancia);
+  
+}
